bf02d4fe12ac6091eb622fbde5a7850c487e5b39
Lucas William <lucaswilliameufrasio@gmail.com>
2021-03-28 19:56:22 -0300

    feat: add GuestHome screen components

--- mobile/src/presentation/screens/guest-home/guest-home.tsx
+++ mobile/src/presentation/screens/guest-home/guest-home.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { StyleSheet, Text, View, SafeAreaView, StatusBar } from 'react-native'
+import { TouchableOpacity } from 'react-native-gesture-handler'
 
 export default function GuestHome (): JSX.Element {
   return (
@@ -8,7 +9,10 @@ export default function GuestHome (): JSX.Element {
         <Text style={styles.headerLeading}>MiniSIGAA</Text>
       </View>
       <View style={styles.content}>
-        <Text>Mini Academic Management System!</Text>
+        <Text style={styles.contentTitle}>Bem vindo!</Text>
+        <TouchableOpacity style={styles.contentButton} onPress={() => {}}>
+          <Text style={styles.contentButtonText}>Acessar o sistema</Text>
+        </TouchableOpacity>
       </View>
     </SafeAreaView>
   )
@@ -23,8 +27,30 @@ const styles = StyleSheet.create({
 
   content: {
     alignItems: 'center',
-    height: '100%',
+    flex: 1,
     justifyContent: 'center'
+  },
+
+  contentButton: {
+    alignItems: 'center',
+    borderColor: '#000',
+    borderRadius: 8,
+    borderWidth: 1,
+    height: 60,
+    justifyContent: 'center',
+    marginTop: 10,
+    width: 260
+  },
+
+  contentButtonText: {
+    fontFamily: 'Roboto_700Bold',
+    fontSize: 16
+  },
+
+  contentTitle: {
+    color: '#000',
+    fontFamily: 'RedHatDisplay_900Black',
+    fontSize: 24
   },
 
   header: {
