738fc0f1579dabcd5eafbefd72b5790b9d0b309a
Lucas William <lucaswilliameufrasio@gmail.com>
2021-03-28 19:48:50 -0300

    feat: add GuestHome screen components

--- mobile/src/presentation/screens/guest-home/guest-home.tsx
+++ mobile/src/presentation/screens/guest-home/guest-home.tsx
@@ -1,19 +1,40 @@
 import React from 'react'
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, Text, View, SafeAreaView, StatusBar } from 'react-native'
 
 export default function GuestHome (): JSX.Element {
   return (
-    <View style={styles.container}>
-      <Text>Mini Academic Management System!</Text>
-    </View>
+    <SafeAreaView style={styles.container}>
+      <View style={styles.header}>
+        <Text style={styles.headerLeading}>MiniSIGAA</Text>
+      </View>
+      <View style={styles.content}>
+        <Text>Mini Academic Management System!</Text>
+      </View>
+    </SafeAreaView>
   )
 }
 
 const styles = StyleSheet.create({
   container: {
-    alignItems: 'center',
     backgroundColor: '#fff',
     flex: 1,
+    paddingTop: StatusBar.currentHeight
+  },
+
+  content: {
+    alignItems: 'center',
+    height: '100%',
     justifyContent: 'center'
+  },
+
+  header: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    width: '100%'
+  },
+
+  headerLeading: {
+    fontFamily: 'RedHatDisplay_900Black',
+    marginLeft: 10
   }
 })
