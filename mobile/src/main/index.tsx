ffd346415a44345faaf3da90ffc8e9f443240715
Lucas William <lucaswilliameufrasio@gmail.com>
2021-03-28 19:22:48 -0300

    feat: add status bar style and background color

--- mobile/src/main/index.tsx
+++ mobile/src/main/index.tsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Router from '@/main/routes/router'
+import { StatusBar } from 'react-native'
 
 const Main: React.FC = () => {
   return (
     <>
       <Router />
+      <StatusBar barStyle="light-content"/>
     </>
   )
 }
