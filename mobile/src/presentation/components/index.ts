17a1d8c3610f9977057435756ec408dd5efb2176
Lucas William <lucaswilliameufrasio@gmail.com>
2021-03-29 12:46:50 -0300

    feat: add GuestHeader component

--- mobile/src/presentation/components/index.ts
+++ mobile/src/presentation/components/index.ts
@@ -1,3 +1,4 @@
 export { default as Header } from './header/header'
+export { default as GuestHeader } from './guest-header/guest-header'
 export { default as ScreenWrapper } from './screen-wrapper/screen-wrapper'
 export { default as CheckBox } from './checkbox/checkbox'
