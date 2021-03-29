c78bd8a8d13313b635000b45f49b0daf5a2c8134
Lucas William <lucaswilliameufrasio@gmail.com>
2021-03-29 13:59:59 -0300

    chore: add jest assets transformer

--- mobile/jest.config.js
+++ mobile/jest.config.js
@@ -8,6 +8,10 @@ module.exports = {
     '@testing-library/jest-native/extend-expect',
     './node_modules/react-native-gesture-handler/jestSetup.js'
   ],
+  moduleNameMapper: {
+    '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
+      '<rootDir>/assetsTransformer.js'
+  },
   coverageProvider: 'babel',
   testEnvironment: 'jsdom'
 }
