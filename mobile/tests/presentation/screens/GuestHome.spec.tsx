bb919b9e89057e89dcfd585167bb19e2ea3e7f10
Lucas William <lucaswilliameufrasio@gmail.com>
2021-03-29 14:00:23 -0300

    refactor: move and rename test file

--- mobile/tests/presentation/screens/GuestHome.spec.tsx
+++
@@ -1,8 +0,0 @@
-import 'react-native'
-import React from 'react'
-import { render } from '@testing-library/react-native'
-import GuestHome from '@/presentation/screens/GuestHome'
-
-it('renders correctly', () => {
-  render(<GuestHome />)
-})
