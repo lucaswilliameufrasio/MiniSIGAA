35977f9d36b2ae3f52555a84451feb68a5683de7
Lucas William <lucaswilliameufrasio@gmail.com>
2021-03-29 12:44:59 -0300

    feat: add AdvisorHome screen

--- mobile/src/main/routes/router.tsx
+++ mobile/src/main/routes/router.tsx
@@ -1,7 +1,12 @@
 import React from 'react'
 import { NavigationContainer } from '@react-navigation/native'
 import { createStackNavigator } from '@react-navigation/stack'
-import { AdvisorTeacherManagement, GuestHome, Login } from '@/presentation/screens'
+import {
+  AdvisorHome,
+  AdvisorTeacherManagement,
+  GuestHome,
+  Login
+} from '@/presentation/screens'
 
 const Stack = createStackNavigator()
 
@@ -9,14 +14,18 @@ const Router: React.FC = () => {
   return (
     <NavigationContainer>
       <Stack.Navigator
-        initialRouteName='AdvisorTeacherManagement'
+        initialRouteName='AdvisorHome'
         screenOptions={{
           headerShown: false
         }}
       >
         <Stack.Screen name='GuestHome' component={GuestHome}></Stack.Screen>
         <Stack.Screen name='Login' component={Login}></Stack.Screen>
-        <Stack.Screen name='AdvisorTeacherManagement' component={AdvisorTeacherManagement}></Stack.Screen>
+        <Stack.Screen name='AdvisorHome' component={AdvisorHome}></Stack.Screen>
+        <Stack.Screen
+          name='AdvisorTeacherManagement'
+          component={AdvisorTeacherManagement}
+        ></Stack.Screen>
       </Stack.Navigator>
     </NavigationContainer>
   )
--- mobile/src/presentation/screens/index.ts
+++ mobile/src/presentation/screens/index.ts
@@ -1,3 +1,4 @@
 export { default as GuestHome } from './guest-home/guest-home'
 export { default as Login } from './login/login'
+export { default as AdvisorHome } from './advisor-home/advisor-home'
 export { default as AdvisorTeacherManagement } from './advisor-teacher-management/advisor-teacher-management'
