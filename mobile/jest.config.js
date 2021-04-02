781af232bca3a793ebc088d442a3fd139e6bbc47
Lucas William <lucaswilliameufrasio@gmail.com>
2021-04-02 19:09:52 -0300

    chore: change the preset of jest

--- mobile/jest.config.js
+++ mobile/jest.config.js
@@ -3,7 +3,7 @@ module.exports = {
   collectCoverageFrom: ['<rootDir>/src/**/*.{ts,tsx}'],
   coverageDirectory: 'coverage',
   testPathIgnorePatterns: ['<rootDir>/node_modules/'],
-  preset: 'react-native',
+  preset: 'jest-expo',
   setupFilesAfterEnv: [
     '@testing-library/jest-native/extend-expect',
     './node_modules/react-native-gesture-handler/jestSetup.js'
