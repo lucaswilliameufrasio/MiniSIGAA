0c2da553a83930100358105b2d223b4cf8ab6e90
Lucas William <lucaswilliameufrasio@gmail.com>
2021-04-26 07:14:00 -0300

    refactor: modify DbAuthentication to use Argon2Adapter as HashComparer

--- api/src/main/factories/usecases/db-authentication-factory.ts
+++ api/src/main/factories/usecases/db-authentication-factory.ts
@@ -1,23 +1,23 @@
 import { getEnv } from '@/main/config/env'
 import { LoadPersonByEmailKnexRepository , LoadAdvisorByPersonIdKnexRepository, LoadTeacherByPersonIdKnexRepository, LoadStudentByPersonIdKnexRepository } from '@/infra/repositories'
-import { BcryptAdapter } from '@/infra/cryptography/bcrypt-adapter'
 import { JwtAdapter } from '@/infra/cryptography/jwt-adapter'
 import { DbAuthentication } from '@/data/usecases/db-authentication'
 import { Authentication } from '@/domain/usecases'
+import { Argon2Adapter } from '@/infra/cryptography/argon2-adapter'
 
 export const makeDbAuthentication = (): Authentication => {
   const loadPersonByEmailRepository = new LoadPersonByEmailKnexRepository()
   const loadTeacherByPersonIdRepository = new LoadTeacherByPersonIdKnexRepository()
   const loadStudentByPersonIdRepository = new LoadStudentByPersonIdKnexRepository()
   const loadAdvisorByPersonIdRepository = new LoadAdvisorByPersonIdKnexRepository()
-  const bcryptAdapter = new BcryptAdapter()
+  const argon2Adapter = new Argon2Adapter()
   const jwtAdapter = new JwtAdapter(getEnv('jwtSecret'))
   return new DbAuthentication(
     loadAdvisorByPersonIdRepository,
     loadStudentByPersonIdRepository,
     loadTeacherByPersonIdRepository,
     loadPersonByEmailRepository,
-    bcryptAdapter,
+    argon2Adapter,
     jwtAdapter
   )
 }
