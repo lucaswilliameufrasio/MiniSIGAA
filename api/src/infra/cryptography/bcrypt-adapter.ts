d3106793afe8dab7ce08dff25779bda57257a308
Lucas William <lucaswilliameufrasio@gmail.com>
2021-04-26 07:14:51 -0300

    refactor: remove bcrypt

--- api/src/infra/cryptography/bcrypt-adapter.ts
+++
@@ -1,8 +0,0 @@
-import { HashComparer } from '@/data/contracts'
-import bcrypt from 'bcrypt'
-
-export class BcryptAdapter implements HashComparer {
-  async compare (plaintext: string, digest: string): Promise<boolean> {
-    return bcrypt.compare(plaintext, digest)
-  }
-}
