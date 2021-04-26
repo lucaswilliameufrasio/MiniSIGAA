dc4168b7557ab3e0b55a4731f610c20229b0403c
Lucas William <lucaswilliameufrasio@gmail.com>
2021-04-26 07:15:33 -0300

    refactor: organize imports

---
+++ api/src/infra/cryptography/index.ts
@@ -0,0 +1,2 @@
+export * from './argon2-adapter'
+export * from './jwt-adapter'
--- api/src/main/factories/usecases/db-authentication-factory.ts
+++ api/src/main/factories/usecases/db-authentication-factory.ts
@@ -1,9 +1,13 @@
 import { getEnv } from '@/main/config/env'
-import { LoadPersonByEmailKnexRepository , LoadAdvisorByPersonIdKnexRepository, LoadTeacherByPersonIdKnexRepository, LoadStudentByPersonIdKnexRepository } from '@/infra/repositories'
-import { JwtAdapter } from '@/infra/cryptography/jwt-adapter'
+import {
+  LoadPersonByEmailKnexRepository,
+  LoadAdvisorByPersonIdKnexRepository,
+  LoadTeacherByPersonIdKnexRepository,
+  LoadStudentByPersonIdKnexRepository
+} from '@/infra/repositories'
+import { JwtAdapter, Argon2Adapter } from '@/infra/cryptography'
 import { DbAuthentication } from '@/data/usecases/db-authentication'
 import { Authentication } from '@/domain/usecases'
-import { Argon2Adapter } from '@/infra/cryptography/argon2-adapter'
 
 export const makeDbAuthentication = (): Authentication => {
   const loadPersonByEmailRepository = new LoadPersonByEmailKnexRepository()
