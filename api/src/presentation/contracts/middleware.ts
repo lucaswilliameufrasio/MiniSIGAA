80d7e8c1e3402f83a1e5c5e4c651968efec9cbaa
Lucas William <lucaswilliameufrasio@gmail.com>
2021-04-27 11:54:05 -0300

    refactor: fix import

--- api/src/presentation/contracts/middleware.ts
+++ api/src/presentation/contracts/middleware.ts
@@ -1,4 +1,4 @@
-import { HttpResponse } from '@/presentation/protocols'
+import { HttpResponse } from '@/presentation/contracts'
 
 export interface Middleware<T = any> {
   handle: (httpRequest: T) => Promise<HttpResponse>
