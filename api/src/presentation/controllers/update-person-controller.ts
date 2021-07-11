531c05840d7383f8139023f51bd0e7912e781505
Lucas William <lucaswilliameufrasio@gmail.com>
2021-07-10 21:26:48 -0300

    feat: add a temporary required fields validation

--- api/src/presentation/controllers/update-person-controller.ts
+++ api/src/presentation/controllers/update-person-controller.ts
@@ -1,11 +1,35 @@
 import { Controller, HttpResponse } from '@/presentation/contracts'
-import { badRequest, noContent } from '@/presentation/helpers'
+import { badRequest, noContent, serverError } from '@/presentation/helpers'
+import { MissingParamError } from '@/presentation/errors'
 import { UpdatePerson } from '@/domain/usecases'
 
 export class UpdatePersonController implements Controller {
   constructor (private readonly updateTeacher: UpdatePerson) {}
 
   async handle (request: UpdatePersonController.Request): Promise<HttpResponse> {
+    const requiredFields = [
+      'person_id',
+      'name',
+      'email',
+      'cpf',
+      'sex',
+      'age',
+      'city',
+      'state',
+      'street',
+      'house_number'
+    ]
+
+    for (const field of requiredFields) {
+      if (
+        request[field] === undefined ||
+        request[field] === '' ||
+        request[field] === null
+      ) {
+        return badRequest(new MissingParamError(field))
+      }
+    }
+
     try {
       await this.updateTeacher.execute({
         id: request.person_id,
@@ -24,7 +48,8 @@ export class UpdatePersonController implements Controller {
 
       return noContent()
     } catch (error) {
-      return badRequest(error)
+      console.log(error)
+      return serverError(error)
     }
   }
 }
